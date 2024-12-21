276a49834fd572cdc05510170839f030bd244530
root <root@DESKTOP-SHForidAhamed.>
2024-12-21 15:30:15 +0600

    Change package and config files.

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,15 +4,6 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
   build: {
     outDir: 'dist',
   },
