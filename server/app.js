c089bb8a98d2d6a8d0cf65f98ff997cd2627b578
Mehmed Khan <smmehmedkhan@gmail.com>
2025-04-09 12:56:50 +0600

    modifies some dependencies.

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/products', productsRoute);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client/dist')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
   });
