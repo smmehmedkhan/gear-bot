e8c44c93a6293f681f266dc06d4ab7e485e1a492
Mehmed Khan <smmehmedkhan@gmail.com>
2025-04-09 13:05:15 +0600

    modifies some dependencies.

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,8 @@
-import { Container, VStack, Text, SimpleGrid } from '@chakra-ui/react';
-import { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { useProductStore } from '../store/product.js';
-import ProductCard from '../components/ProductCard';
+import { Container, VStack, Text, SimpleGrid } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useProductStore } from "../store/product.js";
+import ProductCard from "../components/ProductCard";
 
 export default function HomePage() {
   const { fetchProducts, products } = useProductStore();
@@ -12,27 +12,27 @@ export default function HomePage() {
   }, [fetchProducts]);
 
   return (
-    <Container maxW={'container.xl'} py={16}>
+    <Container maxW={"container.xl"} py={16}>
       <VStack spacing={8}>
         <Text
           fontSize={{
-            base: '2xl',
-            md: '3xl',
-            lg: '4xl',
+            base: "2xl",
+            md: "3xl",
+            lg: "4xl",
           }}
-          fontWeight={'bold'}
+          fontWeight={"bold"}
           mb={8}
-          textAlign={'center'}
-          textTransform={'capitalize'}
+          textAlign={"center"}
+          textTransform={"capitalize"}
           bgGradient="to-r"
           gradientFrom="blue.500"
           gradientTo="cyan.400"
-          bgClip={'text'}
+          bgClip={"text"}
         >
           Current Products 🚀
         </Text>
 
-        <SimpleGrid columns={{ lg: 3, md: 2, sm: 1 }} gap="2.5rem" w={'full'}>
+        <SimpleGrid columns={{ lg: 3, md: 2, sm: 1 }} gap="2.5rem" w={"full"}>
           {products.map((product) => (
             <ProductCard key={product._id} product={product} />
           ))}
@@ -40,17 +40,17 @@ export default function HomePage() {
 
         {products.length === 0 && (
           <Text
-            fontSize={{ base: 'xl', md: '2xl' }}
-            fontWeight={'bold'}
-            textAlign={'center'}
-            color={'gray.500'}
+            fontSize={{ base: "xl", md: "2xl" }}
+            fontWeight={"bold"}
+            textAlign={"center"}
+            color={"gray.500"}
           >
-            No products found😿{' '}
-            <Link to={'/create'}>
+            No products found😿{" "}
+            <Link to={"/create"}>
               <Text
-                as={'span'}
-                color={'blue.500'}
-                _hover={{ textDecoration: 'underline' }}
+                as={"span"}
+                color={"blue.500"}
+                _hover={{ textDecoration: "underline" }}
               >
                 Create a product
               </Text>
